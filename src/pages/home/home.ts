4d5565956e02921a14909ee972e0eebd0fa88726
Elvis Alcántara Pinedo <diskcom123@gmail.com>
2018-09-07 14:29:17 -0500

    subir
    xD

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -6,7 +6,10 @@ import { StreamingMedia, StreamingVideoOptions } from '@ionic-native/streaming-m
   templateUrl: 'home.html'
 })
 export class HomePage {
-  video: string = "https://secure-playlist.livestream.com/streams/13907141_4117932_lsiuzh3hv37qvmycbrw_1/media/13907141_4117932_lsiuzh3hv37qvmycbrw_1@678000pb.m3u8?dw=14400&token=1536346305_436d326fae2c1f82ce27383f5e9ee380bf797b9d&ts=1536170400";
+  //video: string = "https://secure-playlist.livestream.com/streams/13907141_4117932_lsiuzh3hv37qvmycbrw_1/media/13907141_4117932_lsiuzh3hv37qvmycbrw_1@678000pb.m3u8?dw=14400&token=1536346305_436d326fae2c1f82ce27383f5e9ee380bf797b9d&ts=1536170400";
+  //video: string = "https://secure-playlist.livestream.com/streams/888332_931293_lsieeuprxuswytz7wtt_1/media/888332_931293_lsieeuprxuswytz7wtt_1@2320000p.m3u8?dw=14400&token=1536438713_ea67a9ee2d0da48d05735053d1fd7bfabd3bc6ac&ts=1536264000";
+  //video: string = "https://secure-playlist.livestream.com/streams/14174136_8329774_lsiul6xv22hwjlbjyml_1/media/14174136_8329774_lsiul6xv22hwjlbjyml_1@678000pb.m3u8?dw=14400&token=1536439756_02eff3b81fe63853898ce97a5ddc6590abf0a93c&ts=1536264000";
+  video:string ="https://livestream.com/accounts/16944724/events/5883280/videos/130121633";
   constructor(public navCtrl: NavController,private streamingMedia: StreamingMedia) {
 
   }
